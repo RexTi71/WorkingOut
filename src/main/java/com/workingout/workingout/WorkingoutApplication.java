d2bb224f1d9da4af71571748c0bed6357a848b0f
Dominik Rekowski <generator603@gmail.com>
2026-02-12 16:03:43 +0100

    Initial Commit

---
+++ src/main/java/com/workingout/workingout/WorkingoutApplication.java
@@ -0,0 +1,13 @@
+package com.workingout.workingout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingoutApplication.class, args);
+	}
+
+}
